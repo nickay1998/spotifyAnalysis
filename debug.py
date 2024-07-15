692fbcb3203e40f26936c53a71989a61653c614c
Nicolas Au-Yeung <nickay1998@gmail.com>
2024-07-16 00:15:04 +0800

    made debugging with streamlit easier

---
+++ debug.py
@@ -0,0 +1,16 @@
+import os
+import streamlit.web.bootstrap as bootstrap
+import streamlit.runtime.scriptrunner.magic_funcs
+
+if __name__ == "__main__":
+    os.chdir(os.path.dirname(__file__))
+
+    flag_options = {
+        "server.port": 8501,
+        "global.developmentMode": False
+    }
+
+    bootstrap.load_config_options(flag_options=flag_options)
+    flag_options["-is_running_with_streamlit"] = True
+
+    bootstrap.run("./streamlit_app.py", False, ["streamlit run"], flag_options)
